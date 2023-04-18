4bb76db4d16082b06ff02a83a5c9307666e4ec6e
mewluee <yunide2@naver.com>
2023-04-18 10:23:28 +0900

    first commit

---
+++ src/main/java/com/example/qaboard/QAboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.qaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qaboard/QAboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QAboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
